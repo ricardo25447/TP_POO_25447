02ff3110e513af021a0f668b15ac9b5a42e7665f
ricardo25447 <a25447@alunos.ipca.pt>
2024-10-19 15:54:28 +0100

    Criação das classes, atributos e construtores. Próximo passo será criar ligação entre classes e elaborar o program.

---
+++ TP_POO_25447/Contract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_POO_25447
+{
+    internal class Contract
+    {
+        //attributes
+        public DateTime ContractStartDate { get; set; }
+        public DateTime ContractEndDate { get; set; }
+        public double MonthlyAmount { get; set; }
+
+        //constructor
+        public Contract(DateTime contractStartDate, DateTime contractEndDate, double monthlyAmount)
+        {
+            ContractStartDate = contractStartDate;
+            ContractEndDate = contractEndDate;
+            MonthlyAmount = monthlyAmount;
+        }
+    }
+}
